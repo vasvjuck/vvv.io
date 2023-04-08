201e4a8da9b1cf7b642b6c66cd252977fc946987
Vasyl Vasiuk <vasyl.vasiuk@moonshiner.at>
2023-04-08 22:33:43 +0200

    fix: correct import

--- components/Halo.js
+++ components/Halo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import useMousePosition from "hooks/usemouseposition";
+import useMousePosition from "hooks/useMousePosition";
 import { motion } from "framer-motion";
 import cn from "clsx";
 
