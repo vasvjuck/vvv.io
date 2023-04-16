957b65335e50d91a0a70d4db6921d79843144f4a
Vasyl Vasiuk <vasyl.vasiuk@moonshiner.at>
2023-04-16 20:39:12 +0200

    fix: onClick close menu

--- components/header.js
+++ components/header.js
@@ -67,7 +67,6 @@ const Header = () => {
                             <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
                         </svg>
                     </Popover.Button>
-
                     <Transition
                         as={Fragment}
                         enter="transition ease-out duration-200"
@@ -78,21 +77,23 @@ const Header = () => {
                         leaveTo="opacity-0 translate-y-1"
                     >
                         <Popover.Panel className="absolute z-10 right-0 p-2 mt-2 overflow-auto text-base origin-top-right shadow-lg w-40 rounded-xl bg-blur backdrop-blur-lg focus:outline-none sm:text-sm">
-                            <div className="grid">
-                                {links.map((link) => (
-                                    <Link
-                                        key={link.href}
-                                        href={link.href}
-                                        className={clsx(
-                                            "px-4 py-2 rounded-md hover:text-primary transition-colors",
-                                            pathname === link.href ? "bg-secondaryA font-medium" : "font-normal"
-                                        )}
-
-                                    >
-                                        {link.label}
-                                    </Link>
-                                ))}
-                            </div>
+                            {({ close }) => (
+                                <div className="grid">
+                                    {links.map((link) => (
+                                        <Link
+                                            key={link.href}
+                                            href={link.href}
+                                            className={clsx(
+                                                "px-4 py-2 rounded-md hover:text-primary transition-colors",
+                                                pathname === link.href ? "bg-secondaryA font-medium" : "font-normal"
+                                            )}
+                                            onClick={() => close()}
+                                        >
+                                            {link.label}
+                                        </Link>
+                                    ))}
+                                </div>
+                            )}
                         </Popover.Panel>
                     </Transition>
                 </Popover>
